af123ac59ffcfbd6f8ef1a7f8c1c29945fff9200
John Krug <john@Johns-MacBook-Pro.local>
2017-01-23 17:37:57 -0500

    working spotify/movies/twitter

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'RNsxTTXGALYAuJBM4AKxIDcYq',
+  consumer_secret: 'pPtoAnDazGOUfpASsCKQMvmxrXsVRRiuXlUcPJANSr38NCRzpg',
+  access_token_key: '823311933596205057-oH9dZuSp3LkeQPK7T9mc2J536uSaKeY',
+  access_token_secret: 'etfOKXI7ph2z8XS6IPAVRbdvWAtMg9kKrEKktfjvoyPHw',
+}
